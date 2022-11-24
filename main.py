07ff3bcbe1e1360e23ab4e5514a5355e10225ee6
Diogo Lacerda <116228981+Diogolcs@users.noreply.github.com>
2022-11-24 00:48:47 +0000

    .2

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 
 from model.Pessoa import Pessoa
+from database.Database import Database
+from dao.PessoaDAO import PessoaDAO
 
 #Exemplo de uso
 aldair = Pessoa(1, "Aldair Frederico")
@@ -7,3 +9,20 @@ print(aldair)
 
 #Quero mostrar só o nome
 print(aldair.nome)
+
+print("Daqui pra frente é so alegria")
+
+
+#Chama o objeto do banco de dados
+db = Database()
+
+
+PessoaDAO = PessoaDAO( db.conexao, db.cursor)
+
+
+pessoas = PessoaDAO.getAll()
+for pessoa in pessoas:
+ print(pessoa)
+
+
+
